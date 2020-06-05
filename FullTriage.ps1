b878fe16b68d5e1016119bc34abb720ecb86d09c
jafarspalace <65368226+jafarspalace@users.noreply.github.com>
2020-06-04 18:33:02 -0700

    Add back Adding MFT to zip file
    Accidentally removed during testing - adding back

--- FullTriage.ps1
+++ FullTriage.ps1
@@ -334,4 +334,13 @@ PS C:\> Export-MFT -ComputerName Server01 -Volume F
     [GC]::Collect()
     $ScriptTime.Stop()
     Write-Verbose "Done, execution time: $($ScriptTime.Elapsed)"
+    
+    <# Compress all the things #>
+    echo "Adding MFT to Full Collection Archive"
+    Get-ChildItem -Path $Env:SystemDrive\Collections\MFTOUT.bin | Compress-Archive -Update -DestinationPath $Env:SystemDrive\Collections\FullCollection.zip
+
+    <# Clean up MFT #>
+    echo "Cleaning up"
+    Get-ChildItem -Path $Env:SystemDrive\Collections -Exclude FullCollection.zip | foreach { Remove-Item -Path $_.FullName -Recurse -Force}
+    echo "All done now"
 }
